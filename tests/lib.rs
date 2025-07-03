db3da54138ce1e346577e285e5f3402f3b5691da
ismail <dev.1smail.eke34@gmail.com>
2025-07-03 23:23:03 +0300

    1.21.90

--- tests/lib.rs
+++ tests/lib.rs
@@ -5,7 +5,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_work_function() {
-        let mut bedrock = bedrock::new("1.21.80".to_string(), false);
+        let mut bedrock = bedrock::new("1.21.90".to_string(), false);
 
         bedrock.set_auth_callback(|code, url| {
             println!("You can log in with the code {} at {}", code, url);
